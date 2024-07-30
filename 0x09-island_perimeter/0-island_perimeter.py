e4e93b74d4bb9b2f20fd30c18832ff37dab20ba9
ayoub <sb.naba2@gmail.com>
2024-07-30 21:11:54 +0100

    island primeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,16 +14,12 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                #check up
                 if r == 0 or grid[r-1][c] == 0:
                     perimeter += 1
-                #check down
                 if r == rows - 1 or grid[r+1][c] == 0:
                     perimeter += 1
-                #check left
                 if c == 0 or grid[r][c-1] == 0:
                     perimeter += 1
-                #check right
                 if c == cols - 1 or grid[r][c+1] ==0:
                     perimeter += 1
     return perimeter
